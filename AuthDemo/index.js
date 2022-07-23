d7abe2e15f4336e6761a97dc510b18be9bd69393
unknown <surajverma8787@gmail.com>
2022-07-23 15:38:17 +0530

    Registering

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
 const User = require("./models/user");
+const bcrypt = require("bcrypt");
+mongoose.connect('mongodb://localhost:27017/authD')
+    .then(() => {
+        console.log("Mongo connection created");
+    })
+    .catch((err) => {
+        console.log("Mongo Connection Failed");
+        console.log(err);
+    })
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 app.get("/register", (req, res) => {
     res.render('register');
+})
+app.get("/", (req, res) => {
+    res.send("Home Page");
+})
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    //Here 12 is number of salt rounds
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect("/");
 })
 app.get('/secret', (req, res) => {
     res.send("Secret revealed");
